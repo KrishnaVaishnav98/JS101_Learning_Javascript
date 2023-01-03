bae27061d8973dacb2e20d7b1d65e891d6f0b2b1
VIDHI VAISHNAV <vidhivaishnav@VIDHIs-MacBook-Air.local>
2023-01-03 22:50:49 +0530

    mathematical operator

---
+++ Mathematical-operators/1.js
@@ -0,0 +1,13 @@
+// You are given three numbers, stored in variables with the following names
+// one, two, three
+// You have to find the square of the three numbers, and find the sum of their square values
+// Note : Square of a number, is a number multiplied by itself. For example, the square of 3, is 3 * 3 = 9
+
+
+
+
+function squareAndSum(one, two, three) {
+console.log(one*one+two*two+three*three);
+
+
+}
